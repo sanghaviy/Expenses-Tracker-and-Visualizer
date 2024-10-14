26cafed82797ea692e3d37ee269a0f7385784387
Mahimachennupati <mahimachowdary2001@gmail.com>
2024-10-13 23:08:27 -0500

    feature: implemented complete categorization funcationality

--- src/app/components/add-expenses/add-expenses.component.ts
+++ src/app/components/add-expenses/add-expenses.component.ts
@@ -24,7 +24,12 @@ interface Currency {
 })
 export class AddExpensesComponent implements OnInit {
   expenseForm!: FormGroup;
-  categories: string[] = ['Groceries', 'Insurance', 'Entertainment', 'Dining out'];
+  categories: string[] = [
+    'Groceries',
+    'Insurance',
+    'Entertainment',
+    'Dining out',
+  ];
   temporaryCategories: string[] = [];
   newCategory: string = '';
   loggedInUser: any;
@@ -43,7 +48,7 @@ export class AddExpensesComponent implements OnInit {
   totalSpent: number = 0;
 
   constructor(private toastr: ToastrService, private fb: FormBuilder) {
-    debugger
+    debugger;
     this.selectedCurrency = this.currencies[0];
   }
 
@@ -68,8 +73,12 @@ export class AddExpensesComponent implements OnInit {
   }
 
   loadCategories() {
-    this.loggedInUser = JSON.parse(localStorage.getItem('loggedInUser') || '{}');
-    const storedCategories = localStorage.getItem(`categories_${this.loggedInUser.username}`);
+    this.loggedInUser = JSON.parse(
+      localStorage.getItem('loggedInUser') || '{}'
+    );
+    const storedCategories = localStorage.getItem(
+      `categories_${this.loggedInUser.username}`
+    );
     if (storedCategories) {
       try {
         this.categories = JSON.parse(storedCategories);
@@ -86,7 +95,10 @@ export class AddExpensesComponent implements OnInit {
       this.categories.push(trimmedCategory);
       this.newCategory = '';
     } else {
-      this.toastr.error('Please enter a valid, non-duplicate category name.', 'Error');
+      this.toastr.error(
+        'Please enter a valid, non-duplicate category name.',
+        'Error'
+      );
     }
   }
 
@@ -98,8 +110,13 @@ export class AddExpensesComponent implements OnInit {
   }
 
   saveCategories() {
-    const uniqueCategories = Array.from(new Set([...this.categories, ...this.temporaryCategories]));
-    localStorage.setItem(`categories_${this.loggedInUser.username}`, JSON.stringify(uniqueCategories));
+    const uniqueCategories = Array.from(
+      new Set([...this.categories, ...this.temporaryCategories])
+    );
+    localStorage.setItem(
+      `categories_${this.loggedInUser.username}`,
+      JSON.stringify(uniqueCategories)
+    );
     this.toastr.success('Categories saved successfully!', 'Success');
     this.temporaryCategories = [];
   }
@@ -107,7 +124,10 @@ export class AddExpensesComponent implements OnInit {
   submitExpense() {
     if (this.expenseForm.valid) {
       if (this.monthlyBudget === 0) {
-        this.toastr.error('Please set a monthly budget before adding expenses.', 'Error');
+        this.toastr.error(
+          'Please set a monthly budget before adding expenses.',
+          'Error'
+        );
         return;
       }
 
@@ -117,21 +137,30 @@ export class AddExpensesComponent implements OnInit {
       this.resetExpenseForm();
       this.calculateSpendingPercentage();
     } else {
-      this.toastr.error('Please fill all the required fields correctly.', 'Error');
+      this.toastr.error(
+        'Please fill all the required fields correctly.',
+        'Error'
+      );
     }
   }
 
   saveExpenseToLocalStorage(expense: Expense) {
-    this.loggedInUser = JSON.parse(localStorage.getItem('loggedInUser') || '{}');
+    this.loggedInUser = JSON.parse(
+      localStorage.getItem('loggedInUser') || '{}'
+    );
     const userExpensesKey = `expenses_${this.loggedInUser.username}`;
-    const existingExpenses = JSON.parse(localStorage.getItem(userExpensesKey) || '[]');
+    const existingExpenses = JSON.parse(
+      localStorage.getItem(userExpensesKey) || '[]'
+    );
     existingExpenses.push(expense);
     localStorage.setItem(userExpensesKey, JSON.stringify(existingExpenses));
     this.loadExpenses();
   }
 
   loadExpenses() {
-    this.loggedInUser = JSON.parse(localStorage.getItem('loggedInUser') || '{}');
+    this.loggedInUser = JSON.parse(
+      localStorage.getItem('loggedInUser') || '{}'
+    );
     const userExpensesKey = `expenses_${this.loggedInUser.username}`;
     this.expenses = JSON.parse(localStorage.getItem(userExpensesKey) || '[]');
     this.calculateSpendingPercentage();
@@ -148,45 +177,54 @@ export class AddExpensesComponent implements OnInit {
     this.selectedCurrency = this.selectedCurrencyValue;
 
     const budgetData = {
-        monthlyBudget: this.monthlyBudget,
-        selectedCurrency: this.selectedCurrency,
+      monthlyBudget: this.monthlyBudget,
+      selectedCurrency: this.selectedCurrency,
     };
 
     this.monthlyBudgetValue = this.monthlyBudget;
-    this.calculateSpendingPercentage()
+    this.calculateSpendingPercentage();
 
     localStorage.setItem('budgetData', JSON.stringify(budgetData));
     this.toastr.success('Budget and Currency saved successfully!', 'Success');
-}
+  }
 
-loadBudgetAndCurrency() {
-  const savedBudgetData = localStorage.getItem('budgetData');
-  if (savedBudgetData) {
-    try {
-      const { monthlyBudget, selectedCurrency } = JSON.parse(savedBudgetData);
-      this.monthlyBudget = monthlyBudget || 0;
-      this.selectedCurrency = selectedCurrency || { symbol: '$', name: 'USD' };
-      this.monthlyBudgetValue = this.monthlyBudget;
+  loadBudgetAndCurrency() {
+    const savedBudgetData = localStorage.getItem('budgetData');
+    if (savedBudgetData) {
+      try {
+        const { monthlyBudget, selectedCurrency } = JSON.parse(savedBudgetData);
+        this.monthlyBudget = monthlyBudget || 0;
+        this.selectedCurrency = selectedCurrency || {
+          symbol: '$',
+          name: 'USD',
+        };
+        this.monthlyBudgetValue = this.monthlyBudget;
 
-      // Find the corresponding currency object from the currencies array
-      this.selectedCurrencyValue = this.currencies.find(
-        currency => currency.symbol === this.selectedCurrency.symbol && currency.name === this.selectedCurrency.name
-      ) || { symbol: '$', name: 'USD' }; // Default fallback
-
-    } catch (error) {
-      this.toastr.error('Failed to load budget data.', 'Error');
+        // Find the corresponding currency object from the currencies array
+        this.selectedCurrencyValue = this.currencies.find(
+          (currency) =>
+            currency.symbol === this.selectedCurrency.symbol &&
+            currency.name === this.selectedCurrency.name
+        ) || { symbol: '$', name: 'USD' }; // Default fallback
+      } catch (error) {
+        this.toastr.error('Failed to load budget data.', 'Error');
+      }
     }
   }
-}
 
   calculateSpendingPercentage() {
-    debugger
-    this.totalSpent = this.expenses.reduce((acc, expense) => acc + expense.totalAmount, 0);
+    this.totalSpent = this.expenses.reduce(
+      (acc, expense) => acc + expense.totalAmount,
+      0
+    );
     this.spendingPercentage = (this.totalSpent / this.monthlyBudget) * 100 || 0;
   }
 
   getSpendingColor(): string {
-    const currentSpending = this.expenses.reduce((acc, expense) => acc + expense.totalAmount, 0);
+    const currentSpending = this.expenses.reduce(
+      (acc, expense) => acc + expense.totalAmount,
+      0
+    );
     if (currentSpending < this.monthlyBudget) {
       return 'green';
     } else if (currentSpending === this.monthlyBudget) {
